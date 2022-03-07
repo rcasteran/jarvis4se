rcasteran/jarvis4se

commit aeb76c229ddb4c147f56392b2353b5452b704a16

- Closes #12

src/jarvis/orchestrator.py
@@ -2318,7 +2318,13 @@ def check_add_allocation(allocation_str_list, xml_fun_elem_list, xml_state_list,
                             if elem[1] == fun.name or elem[1] == fun.alias:
                                 check_allocation = \
                                     question_answer.get_allocation_object(fun, xml_fun_elem_list)
-                                if check_allocation is None:
+                                count = None
+                                if check_allocation is not None:
+                                    count = len(check_allocation)
+                                    for item in check_allocation:
+                                        if check_parentality(item, fun_elem):
+                                            count -= 1
+                                if count in (None, 0):
                                     fun_elem.add_allocated_function(fun.id)
                                     fun_elem_allocated_function_list.append([fun_elem, fun])
 
@@ -2329,7 +2335,13 @@ def check_add_allocation(allocation_str_list, xml_fun_elem_list, xml_state_list,
                             if elem[1] == state.name or elem[1] == state.alias:
                                 check_allocation = \
                                     question_answer.get_allocation_object(state, xml_fun_elem_list)
-                                if check_allocation is None:
+                                count = None
+                                if check_allocation is not None:
+                                    count = len(check_allocation)
+                                    for item in check_allocation:
+                                        if check_parentality(item, fun_elem):
+                                            count -= 1
+                                if count in (None, 0):
                                     fun_elem.add_allocated_state(state.id)
                                     fun_elem_allocated_state_list.append([fun_elem, state])
 
@@ -2340,11 +2352,11 @@ def check_add_allocation(allocation_str_list, xml_fun_elem_list, xml_state_list,
                             if elem[1] == fun.name or elem[1] == fun.alias:
                                 check_allocation = \
                                     question_answer.get_allocation_object(fun, xml_state_list)
-                                if not check_allocation:
+                                if check_allocation is None:
                                     state.add_allocated_function(fun.id)
                                     state_allocated_function_list.append([state, fun])
-                                elif check_allocation:
-                                    if not any(state.name in s for s in check_allocation):
+                                else:
+                                    if not any(state in s for s in check_allocation):
                                         state.add_allocated_function(fun.id)
                                         state_allocated_function_list.append([state, fun])
             else:

src/jarvis/question_answer.py
@@ -215,7 +215,7 @@ def matched_allocated(object_str, **kwargs):
             allocation_list = get_allocation_object(wanted_object, kwargs['xml_fun_elem_list'])
             if allocation_list:
                 object_info += f'"{wanted_object.name}" is allocated to ' \
-                               + ", ".join(list(allocation_list))
+                               + ", ".join([elem.name for elem in allocation_list])
                 return object_info
 
     return None
@@ -229,11 +229,11 @@ def get_allocation_object(wanted_object, fun_elem_list):
     if object_type == 'function':
         for fun_elem in fun_elem_list:
             if any(wanted_object.id in s for s in fun_elem.allocated_function_list):
-                allocation_list.add(fun_elem.name)
+                allocation_list.add(fun_elem)
     elif object_type == 'state':
         for fun_elem in fun_elem_list:
             if any(wanted_object.id in s for s in fun_elem.allocated_state_list):
-                allocation_list.add(fun_elem.name)
+                allocation_list.add(fun_elem)
     if allocation_list:
         return allocation_list
 

commit 84168b6595c08bca7699ef85e843d4ade9b1a9eb

- Closes #14 -> Delete extra-whitespaces + correct bug for allocation of Function to State(introduced by https://github.com/rcasteran/jarvis4se/commit/aeb76c229ddb4c147f56392b2353b5452b704a16)

src/jarvis/jarvis.py
@@ -17,7 +17,6 @@ from . import question_answer
 import xml_adapter
 
 
-
 # The class MUST call this class decorator at creation time
 @magics_class
 class MyMagics(Magics):
@@ -31,6 +30,8 @@ class MyMagics(Magics):
         input_str = sio.getvalue()
         # Delete the '"' from input string, to avoid xml to plantuml errors.
         input_str = input_str.replace('"', "")
+        # Delete extra whitespaces
+        input_str = re.sub(' +', ' ', input_str)
         # Get model's declaration, need a space after "with" otherwise print a message
         xml_name_str = re.match(r"^with (.*)(?=.|\n)", input_str, re.MULTILINE)
         if xml_name_str:

src/jarvis/orchestrator.py
@@ -2356,7 +2356,7 @@ def check_add_allocation(allocation_str_list, xml_fun_elem_list, xml_state_list,
                                     state.add_allocated_function(fun.id)
                                     state_allocated_function_list.append([state, fun])
                                 else:
-                                    if not any(state in s for s in check_allocation):
+                                    if state not in check_allocation:
                                         state.add_allocated_function(fun.id)
                                         state_allocated_function_list.append([state, fun])
             else: