be6bd063b30751d8d34462bc6ca9fd24f0fdc2a5
not2behere <not2behere@gmx.fr>
2022-04-26 12:24:01 +0200

    Added test_fun_elem_decompo_with_no_flow() (see #7)

--- tests/test_decomposition_diagrams.py
+++ tests/test_decomposition_diagrams.py
@@ -1,4 +1,5 @@
 import os
+import io
 from pytest_mock import mocker
 from IPython import get_ipython
 from pathlib import Path
@@ -98,6 +99,127 @@ def test_function_with_childs_decomposition(mocker):
 
     assert all(i in result for i in expected)
     assert len(result) - len(''.join(expected)) == 8*len("\'id: xxxxxxxxxx\n")
+
+    fname = os.path.join("./", file_name + ".xml")
+    path = Path(fname)
+    if path:
+        os.remove(path)
+
+
+def test_fun_elem_decompo_with_no_flow(mocker, monkeypatch):
+    """Notebook equivalent (see Issue_#7):
+    %%jarvis
+    with fun_elem_decompo_with_no_flow
+    F1 is a function
+    F1a is a function
+    F1b is a function
+    F1c is a function
+    F1 is composed of F1a
+    F1 is composed of F1b
+    F1 is composed of F1c
+
+    F1c1 is a function
+    F1c is composed of F1c1
+
+    F2 is a function
+    F2a is a function
+    F2 is composed of F2a
+
+    F3 is a function
+    F3a is a function
+    F3 is composed of F3a
+
+    E1 is a functional element
+    E1a is a functional element
+    E1b is a functional element
+    E1c is a functional element
+    E1 is composed of E1a
+    E1 is composed of E1b
+    E1 is composed of E1c
+    E1c1 is a functional element
+    E1c2 is a functional element
+    E1c is composed of E1c1
+    E1c is composed of E1c2
+
+    E1 allocates F1
+    E1 allocates F2
+    E1a allocates F1a
+    E1a allocates F3a
+    E1b allocates F1b
+    E1c allocates F1c
+    E1c1 allocates F1c1
+
+    show decomposition E1
+     """
+    monkeypatch.setattr('sys.stdin', io.StringIO('y')) # Say yes for adding F3 alloacted to E1
+    spy = mocker.spy(plantuml_adapter, "get_fun_elem_decomposition")
+    ip = get_ipython()
+    my_magic = jarvis.MyMagics(ip)
+    file_name = "fun_elem_decompo_with_no_flow"
+    my_magic.jarvis("", "with %s\n" % file_name +
+                    "F1 is a function\n"
+                    "F1a is a function\n"
+                    "F1b is a function\n"
+                    "F1c is a function\n"
+                    "F1 is composed of F1a\n"
+                    "F1 is composed of F1b\n"
+                    "F1 is composed of F1c\n"
+                    "\n"
+                    "F1c1 is a function\n"
+                    "F1c is composed of F1c1\n"
+                    "\n"
+                    "F2 is a function\n"
+                    "F2a is a function\n"
+                    "F2 is composed of F2a\n"
+                    "\n"
+                    "F3 is a function\n"
+                    "F3a is a function\n"
+                    "F3 is composed of F3a\n"
+                    "\n"
+                    "E1 is a functional element\n"
+                    "E1a is a functional element\n"
+                    "E1b is a functional element\n"
+                    "E1c is a functional element\n"
+                    "E1 is composed of E1a\n"
+                    "E1 is composed of E1b\n"
+                    "E1 is composed of E1c\n"
+                    "E1c1 is a functional element\n"
+                    "E1c2 is a functional element\n"
+                    "E1c is composed of E1c1\n"
+                    "E1c is composed of E1c2\n"
+                    "\n"
+                    "E1 allocates F1\n"
+                    "E1 allocates F2\n"
+                    "E1a allocates F1a\n"
+                    "E1a allocates F3a\n"
+                    "E1b allocates F1b\n"
+                    "E1c allocates F1c\n"
+                    "E1c1 allocates F1c1\n"
+                    "\n"
+                    "show decomposition E1\n")
+
+    # result = plantuml text without "@startuml ... @enduml" tags
+    result = spy.spy_return[1]  # First element from returned values by plantuml_binder()
+    expected = ['component "E1" as e1 <<unknown>>{\n',
+                'object "F2" as f2 <<unknown>>\n',
+                'component "E1b" as e1b <<unknown>>{\n',
+                'object "F1b" as f1b <<unknown>>\n',
+                '}\n',
+                'component "E1a" as e1a <<unknown>>{\n',
+                'object "F1a" as f1a <<unknown>>\n',
+                'object "F3a" as f3a <<unknown>>\n',
+                '}\n',
+                'component "E1c" as e1c <<unknown>>{\n',
+                'component "E1c1" as e1c1 <<unknown>>{\n',
+                'object "F1c1" as f1c1 <<unknown>>\n',
+                '}\n',
+                'component "E1c2" as e1c2 <<unknown>>{\n',
+                '}\n',
+                '}\n',
+                '}\n']
+
+    assert all(i in result for i in expected)
+    assert len(result) - len(''.join(expected)) == 11*len("\'id: xxxxxxxxxx\n")
 
     fname = os.path.join("./", file_name + ".xml")
     path = Path(fname)
