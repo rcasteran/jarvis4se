3f370eefbf9ed2203cfbbf947072330b31d71e6f
not2behere <not2behere@gmx.fr>
2022-04-27 12:45:53 +0200

    Added test_fun_elem_decompo_with_interface()

--- tests/test_decomposition_diagrams.py
+++ tests/test_decomposition_diagrams.py
@@ -225,3 +225,148 @@ def test_fun_elem_decompo_with_no_flow(mocker, monkeypatch):
     path = Path(fname)
     if path:
         os.remove(path)
+
+
+def test_fun_elem_decompo_with_interface(mocker):
+    """Notebook equivalent:
+    %%jarvis
+    with fun_elem_decompo_with_interface
+    F1 is a function
+    F1b is a function
+    F1c is a function
+    F1 is composed of F1b
+    F1 is composed of F1c
+
+    F1c1 is a function
+    F1c is composed of F1c1
+
+    F_ext is a function
+
+    E1 is a functional element
+    E1b is a functional element
+    E1c is a functional element
+    Ext is a functional element
+    E1 is composed of E1c
+    E1 is composed of E1b
+    E1c1 is a functional element
+    E1c is composed of E1c1
+
+    E1 allocates F1
+    E1b allocates F1b
+    E1c allocates F1c
+    E1c1 allocates F1c1
+    Ext allocates F_ext
+
+    A is a data
+    A_2 is a data
+    B is a data
+    C is a data
+
+    Fun_inter_A is a functional interface
+    Fun_inter_A allocates A
+    Fun_inter_A allocates A_2
+    Fun_inter_B is a functional interface
+    Fun_inter_B allocates B
+
+    F1c1 produces A
+    F1c1 produces A_2
+    F1b consumes A
+    F1b consumes A_2
+
+    F_ext produces B
+    F1c1 consumes B
+
+    F1b produces C
+    F_ext consumes C
+
+    E1c exposes Fun_inter_A
+    E1c1 exposes Fun_inter_A
+    E1b exposes Fun_inter_A
+
+    show decomposition E1
+     """
+    spy = mocker.spy(plantuml_adapter, "get_fun_elem_decomposition")
+    ip = get_ipython()
+    my_magic = jarvis.MyMagics(ip)
+    file_name = "fun_elem_decompo_with_interface"
+    my_magic.jarvis("", "with %s\n" % file_name +
+                    "F1 is a function\n"
+                    "F1b is a function\n"
+                    "F1c is a function\n"
+                    "F1 is composed of F1b\n"
+                    "F1 is composed of F1c\n"
+                    "\n"
+                    "F1c1 is a function\n"
+                    "F1c is composed of F1c1\n"
+                    "\n"
+                    "F_ext is a function\n"
+                    "\n"
+                    "E1 is a functional element\n"
+                    "E1b is a functional element\n"
+                    "E1c is a functional element\n"
+                    "Ext is a functional element\n"
+                    "E1 is composed of E1b\n"
+                    "E1 is composed of E1c\n"
+                    "E1c1 is a functional element\n"
+                    "E1c is composed of E1c1\n"
+                    "\n"
+                    "E1 allocates F1\n"
+                    "E1b allocates F1b\n"
+                    "E1c allocates F1c\n"
+                    "E1c1 allocates F1c1\n"
+                    "Ext allocates F_ext\n"
+                    "\n"
+                    "A is a data\n"
+                    "A_2 is a data\n"
+                    "B is a data\n"
+                    "C is a data\n"
+                    "\n"
+                    "Fun_inter_A is a functional interface\n"
+                    "Fun_inter_A allocates A\n"
+                    "Fun_inter_A allocates A_2\n"
+                    "Fun_inter_B is a functional interface\n"
+                    "Fun_inter_B allocates B\n"
+                    "\n"
+                    "F1c1 produces A\n"
+                    "F1c1 produces A_2\n"
+                    "F1b consumes A\n"
+                    "F1b consumes A_2\n"
+                    "\n"
+                    "F_ext produces B\n"
+                    "F1c1 consumes B\n"
+                    "\n"
+                    "F1b produces C\n"
+                    "F_ext consumes C\n"
+                    "\n"
+                    "E1c exposes Fun_inter_A\n"
+                    "E1c1 exposes Fun_inter_A\n"
+                    "E1b exposes Fun_inter_A\n"
+                    "\n"
+                    "show decomposition E1\n")
+
+    # result = plantuml text without "@startuml ... @enduml" tags
+    result = spy.spy_return[1]  # First element from returned values by plantuml_binder()
+    expected = ['component "E1" as e1 <<unknown>>{\n',
+                'component "E1c" as e1c <<unknown>>{\n',
+                'component "E1c1" as e1c1 <<unknown>>{\n',
+                'object "F1c1" as f1c1 <<unknown>>\n',
+                '}\n',
+                '}\n',
+                'component "E1b" as e1b <<unknown>>{\n',
+                'object "F1b" as f1b <<unknown>>\n',
+                '}\n',
+                '}\n',
+                'component "Ext" as ext <<unknown>>{\n',
+                'object "F_ext" as f_ext <<unknown>>\n',
+                '}\n',
+                'f_ext #--> f1c1 : B\n',
+                'f1b #--> f_ext : C\n',
+                'e1b', ' -- ', 'e1c1 ', ': fun_inter_a\n']
+
+    assert all(i in result for i in expected)
+    assert len(result) - len(''.join(expected)) == 8*len("\'id: xxxxxxxxxx\n")
+
+    fname = os.path.join("./", file_name + ".xml")
+    path = Path(fname)
+    if path:
+        os.remove(path)
