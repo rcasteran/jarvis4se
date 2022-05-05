d0d9f03ace8c9df083bde46c0be847b6a7b44b0d
not2behere <not2behere@gmx.fr>
2022-05-05 15:20:44 +0200

    Updated test_functional_interface_input()

--- tests/test_input_cell.py
+++ tests/test_input_cell.py
@@ -209,7 +209,8 @@ def test_functional_interface_input(capsys):
                 "Fun_inter is a functional interface\n",
                 "Color is an attribute\n",
                 "The alias for Fun_inter is FI\n",
-                "Data A has no producer(s) nor consumer(s), not added to Fun_inter\n",
+                "Data A has no producer(s) nor consumer(s) allocated to functional "
+                "elements exposing Fun_inter, A not allocated to Fun_inter\n",
                 "The type of Fun_inter is a_type\n",
                 "Attribute Color for Fun_inter with value pink\n",
                 f"{file_name}.xml updated\n"]
